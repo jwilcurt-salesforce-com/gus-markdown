ec8b49cff4120f3a39d7e8dc88836b861f5e7b25
Jared Wilcurt <jwilcurt@exacttarget.com>
2017-06-06 14:54:09 -0400

    Test Page Redesign

--- src/codepen.js
+++ src/codepen.js
@@ -5,3 +5,50 @@ var $ = jquery;
 $('#enter').keyup(function () {
     $('#myhtml').html(textTransform($('#enter').val()));
 });
+
+var sampleIDs = [
+    '001',
+    '002'
+];
+
+window.sampleData = {
+    's000': ''
+};
+
+var i = 0;
+
+// Recursively make sequential network calls
+function getData () {
+    // '001'
+    var id = sampleIDs[i];
+    // 's001'
+    var sID = 's' + id;
+    // Download samples/001.md
+    $.get('samples/' + id + '.md', function (data) {
+        // store it in window.sampleData.s001
+        window.sampleData[sID] = data;
+        i = i + 1;
+        if (i < sampleIDs.length) {
+            getData();
+        }
+    });
+}
+
+getData();
+
+sampleIDs.forEach(function (sampleID) {
+    // <option value="001">001</option>
+    $('#loaders').append('<option value="' + sampleID + '">' + sampleID + '</option>');
+});
+
+$('#loaders').change(function () {
+    // <option data-id="001"></option>
+    var dataID = $(this).val();
+
+    // text = window.sampleData.001;
+    var text = window.sampleData['s' + dataID];
+
+    document.getElementById('enter').value = text;
+
+    $('#myhtml').html(textTransform($('#enter').val()));
+});
