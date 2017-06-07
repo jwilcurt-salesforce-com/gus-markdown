d7bb0792bc0fed39d1aae9c4a2608c32d100a33f
Charles Osborne <cosborne@cosborne-ltm.internal.salesforce.com>
2017-06-07 13:11:07 -0400

    toggle icon

--- dist/background.js
+++ dist/background.js
@@ -1,11 +1,9 @@
 var run = true;
 chrome.browserAction.onClicked.addListener(function () {
     run = !run;
-    console.log(chrome.runtime.getURL("rolledExtension.js"));
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {run: run}, function(response) {
         console.log(response);
       });
     });
-    chrome.tabs.executeScript(null, {file: chrome.runtime.getURL("rolledExtension.js")});
 });
--- src/gus-markdown-extension.js
+++ src/gus-markdown-extension.js
@@ -1,21 +1,31 @@
 import textTransform from './texttransform.js';
 
-var run;
+var run = true;
+var originalHTML;
 
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
     run = request.run;
     console.log(run);
+    initialize(run);
+    sendResponse({received: true});
 });
 
 function viewingPage (descriptionBox) {
     var descriptionBoxEl = document.getElementById(descriptionBox);
     var descriptionBoxHTML = '';
     if (descriptionBoxEl) {
-        descriptionBoxHTML = descriptionBoxEl.innerHTML;
+        descriptionBoxHTML = originalHTML = descriptionBoxEl.innerHTML;
     }
     if (descriptionBoxHTML.length > 0) {
         descriptionBoxEl.innerHTML = textTransform(descriptionBoxHTML);
     }
+}
+
+function previewEditor (event) {
+    var text = event.target.value || event.target.innerText;
+    console.log('text', text);
+    event.target.prev.innerHTML = textTransform(text);
+    console.log('mdPreview', event.target.prev);
 }
 
 /**
@@ -24,45 +34,72 @@ function viewingPage (descriptionBox) {
  * @param  {object} destinationElement This is an already selected element on the page for the html to be injected
  */
 
-function editingPage (element, destinationElement) {
-    var titleDiv = document.createElement('div');
-    titleDiv.id = 'title-div';
-    titleDiv.className = 'gusFormFieldLeft';
-    titleDiv.style =
-    'padding: 0px;' +
-    'text-align: left;' +
-    'margin: 8px 0px;';
-    var markdownPreviewTitle = document.createElement('label');
-    markdownPreviewTitle.id = 'markdown-preview-title';
-    markdownPreviewTitle.innerHTML = 'Markdown Preview';
-    titleDiv.appendChild(markdownPreviewTitle);
-    destinationElement.appendChild(titleDiv);
-    var markdownPreview = document.createElement('div');
-    markdownPreview.id = 'markdown-preview';
-    markdownPreview.className = 'inlineEditWrite';
-    markdownPreview.style =
-    'background: #FFF;' +
-    'border: 1px solid #CCC;' +
-    'border-radius: 4px;' +
-    'padding: 0px 6px;' +
-    'margin-top: 5px;' +
-    'height: 160px;' +
-    'overflow: auto;' +
-    'line-height: 20px;';
-    destinationElement.appendChild(markdownPreview);
-
-    element.style.height = '160px';
-
-
-    function previewEditor () {
-
-        var text = element.value || element.innerText;
-        markdownPreview.innerHTML = textTransform(text);
+function editingPage (elem, destinationElement) {
+    var td = document.querySelector('#title-div');
+    var mdp = document.querySelector('#markdown-preview');
+    var mdpt = document.querySelector('#markdown-preview-title')
+    var titleDiv, markdownPreview, markdownPreviewTitle;
+    if(td) {
+        titleDiv = td;
+        titleDiv.style.display = "block";
+    }
+    else {
+        titleDiv = document.createElement('div');
+        titleDiv.id = 'title-div';
+        titleDiv.className = 'gusFormFieldLeft';
+        titleDiv.style =
+        'padding: 0px;' +
+        'text-align: left;' +
+        'margin: 8px 0px;';
+    }
+    if(mdp) {
+        markdownPreview = mdp;
+        markdownPreview.style.display = "block";
+    }
+    else {
+        markdownPreview = document.createElement('div');
+        markdownPreview.id = 'markdown-preview';
+        markdownPreview.className = 'inlineEditWrite';
+        markdownPreview.style =
+        'background: #FFF;' +
+        'border: 1px solid #CCC;' +
+        'border-radius: 4px;' +
+        'padding: 0px 6px;' +
+        'margin-top: 5px;' +
+        'height: 160px;' +
+        'overflow: auto;' +
+        'line-height: 20px;';
+    }
+    if(mdpt) {
+        markdownPreviewTitle = mdpt;
+        markdownPreviewTitle.style.display = "block";
+    }
+    else {
+        markdownPreviewTitle = document.createElement('label');
+        markdownPreviewTitle.id = 'markdown-preview-title';
+        markdownPreviewTitle.innerHTML = 'Markdown Preview';
+        titleDiv.appendChild(markdownPreviewTitle);
+        destinationElement.appendChild(titleDiv);
+        destinationElement.appendChild(markdownPreview);
+        elem.prev = markdownPreview;
+        elem.addEventListener('keydown', previewEditor);
+        elem.style.height = '160px';
     }
 
-    element.addEventListener('keydown', previewEditor);
-    previewEditor();
+    var initialKeydown = new CustomEvent("keydown");
+    elem.dispatchEvent(initialKeydown);
 
+}
+
+function viewingReset(elem) {
+    document.getElementById(elem).innerHTML = originalHTML;
+}
+
+function editingReset(elem, destElem) {
+    elem.removeEventListener('keydown', previewEditor);
+    document.querySelector('#title-div').style.display = 'none';
+    document.querySelector('#markdown-preview').style.display = 'none';
+    document.querySelector('#markdown-preview-title').style.display = 'none';
 }
 /*
     clears the markdown box by forcing the script to rerun by giving it a keyboard event,
@@ -88,77 +125,121 @@ function clearMarkDownPreview(){
     element.dispatchEvent(keyboardEvent);
  }
 
-
-// Depending on if it's a Bug or Story, there are different horrendous ID's used on the page
-// So we detect the URL and pass in the correct value to the correct function.
-if (!window.ran && location.href.indexOf('/apex/adm_bugedit') > -1 && location.href.indexOf('gus.lightning.force') > -1) {
-    console.log('bugedit lightning'); // eslint-disable-line no-console
-    var element = document.getElementById('bugEdit:j_id0:workSds:storyWorkForm:dstpInput:inputComponent:inputFieldWithContainer:textAreaDelegate_Details_And_Steps_To_Reproduce__c_rta_body');
-    var destinationElement = element.parentElement;
-    editingPage(element, destinationElement);
-
-} else if (!window.ran && location.href.indexOf('/apex/adm_bugedit') > -1 && location.href.indexOf('gus.lightning.force') == -1) {
-    console.log('bugedit classic'); // eslint-disable-line no-console
-    var element = document.querySelectorAll('iframe')[1];
-    function waitForElem() {
-        if(typeof element == 'undefined') {
-            element = document.querySelectorAll('iframe')[1];
-            setTimeout(waitForElem, 250);
+function initialize(run) {
+    // Depending on if it's a Bug or Story, there are different horrendous ID's used on the page
+    // So we detect the URL and pass in the correct value to the correct function.
+    if (location.href.indexOf('/apex/adm_bugedit') > -1 && location.href.indexOf('gus.lightning.force') > -1) {
+        console.log('bugedit lightning'); // eslint-disable-line no-console
+        var element = document.getElementById('bugEdit:j_id0:workSds:storyWorkForm:dstpInput:inputComponent:inputFieldWithContainer:textAreaDelegate_Details_And_Steps_To_Reproduce__c_rta_body');
+        var destinationElement = element.parentElement;
+        if(run) {
+            editingPage(element, destinationElement);
         }
         else {
-            element = element.contentWindow.document.getElementById('bugWorkPage:bugWorkForm:richDetailsInput:textAreaDelegate_Details_and_Steps_to_Reproduce__c_rta_body');
-            var destinationElement = document.getElementById('richDetailsWrapper');
+            editingReset(element, destinationElement);
+        }
+
+    } else if (location.href.indexOf('/apex/adm_bugedit') > -1 && location.href.indexOf('gus.lightning.force') == -1) {
+        console.log('bugedit classic'); // eslint-disable-line no-console
+        var element = document.querySelectorAll('iframe')[1];
+        function waitForElem() {
+            if(typeof element == 'undefined') {
+                element = document.querySelectorAll('iframe')[1];
+                setTimeout(waitForElem, 250);
+            }
+            else {
+                element = element.contentWindow.document.getElementById('bugWorkPage:bugWorkForm:richDetailsInput:textAreaDelegate_Details_and_Steps_to_Reproduce__c_rta_body');
+                var destinationElement = document.getElementById('richDetailsWrapper');
+                if(run) {
+                    editingPage(element, destinationElement);
+                }
+                else {
+                    editingReset(element, destinationElement);
+                }
+
+            }
+        }
+        waitForElem();
+
+    } else if(location.href.indexOf('/apex/ADM_WorkManager') > -1 && location.href.indexOf('gus.lightning.force') == -1){
+        console.log('bugedit classic preview'); // eslint-disable-line no-console
+        setTimeout(function(){}, 200);
+        var element = document.getElementById('descriptionInput');
+        if(element != null){
+            destinationElement = element.parentElement;
             editingPage(element, destinationElement);
-
+            var saveButton = document.getElementById('workSaveButton');
+            var cancelButton = document.getElementById('workCancelButton');
+            if(cancelButton.addEventListener){
+                cancelButton.addEventListener("click", clearMarkDownPreview, false);
+            }
+            if(saveButton.addEventListener){
+                saveButton.addEventListener("click", clearMarkDownPreview, false);
+            }
         }
+
+    } else if (location.href.indexOf('/apex/adm_userstoryedit') > -1 && location.href.indexOf('gus.lightning.force') > -1) {
+        console.log('userstoryedit lightning'); // eslint-disable-line no-console
+        var element = document.getElementById('userStoryEdit:j_id0:workSds:storyWorkForm:descriptionInput:inputComponent:inputFieldWithContainer');
+        var destinationElement = element.parentElement;
+        if(run) {
+            editingPage(element, destinationElement);
+        }
+        else {
+            editingReset(element, destinationElement);
+        }
+
+    } else if (location.href.indexOf('/apex/adm_userstoryedit') > -1 && location.href.indexOf('gus.lightning.force') == -1) {
+        console.log('userstoryedit classic'); // eslint-disable-line no-console
+        var element = document.getElementById('userStoryWorkPage:storyWorkForm:detailsInput:formRow:input');
+        var destinationElement = element.parentElement;
+        if(run) {
+            editingPage(element, destinationElement);
+        }
+        else {
+            editingReset(element, destinationElement);
+        }
+
+    } else if (location.href.indexOf('/apex/adm_userstorydetail') > -1 && location.href.indexOf('gus.lightning.force') > -1) {
+        console.log('userstorydetail lightning'); // eslint-disable-line no-console
+        if(run) {
+            viewingPage('userStoryDetailPage_userStoryWorkForm_detailsInput_inputComponent_outputStandalone_ileinner');
+        }
+        else {
+            viewingReset('userStoryDetailPage_userStoryWorkForm_detailsInput_inputComponent_outputStandalone_ileinner');
+        }
+
+    } else if (location.href.indexOf('/apex/adm_userstorydetail') > -1 && location.href.indexOf('gus.lightning.force') == -1) {
+        console.log('userstorydetail classic'); // eslint-disable-line no-console
+        if(run) {
+            viewingPage('userStoryDetailPage_userStoryWorkForm_detailsInput_inputComponent_outputStandalone_ileinner');
+        }
+        else {
+            viewingReset('userStoryDetailPage_userStoryWorkForm_detailsInput_inputComponent_outputStandalone_ileinner');
+        }
+
+    } else if (location.href.indexOf('/apex/adm_bugdetail') > -1 && location.href.indexOf('gus.lightning.force') > -1) {
+        console.log('bugdetail lightning'); // eslint-disable-line no-console
+        if(run) {
+            viewingPage('bugDetailPage:bugWorkForm:j_id89bugDetailPage:bugWorkForm:j_id89_00NB0000000FiIs_div');
+        }
+        else {
+            viewingReset('bugDetailPage:bugWorkForm:j_id89bugDetailPage:bugWorkForm:j_id89_00NB0000000FiIs_div');
+        }
+
+    } else if (location.href.indexOf('/apex/adm_bugdetail') > -1 && location.href.indexOf('gus.lightning.force') == -1) {
+        console.log('bugdetail classic'); // eslint-disable-line no-console
+        if(run) {
+            viewingPage('bugDetailPage:bugWorkForm:j_id89bugDetailPage:bugWorkForm:j_id89_00NB0000000FiIs_div');
+        }
+        else {
+            viewingReset('bugDetailPage:bugWorkForm:j_id89bugDetailPage:bugWorkForm:j_id89_00NB0000000FiIs_div');
+        }
+
+    } else {
+        console.log('not found'); // eslint-disable-line no-console
     }
-    waitForElem();
 
-} else if(!window.ran && location.href.indexOf('/apex/ADM_WorkManager') > -1 && location.href.indexOf('gus.lightning.force') == -1){
-    console.log('bugedit classic'); // eslint-disable-line no-console
-    setTimeout(function(){}, 200);
-    var element = document.getElementById('descriptionInput');
-    if(element != null){
-        destinationElement = element.parentElement;
-        editingPage(element, destinationElement);
-        var saveButton = document.getElementById('workSaveButton');
-        var cancelButton = document.getElementById('workCancelButton');
-        if(cancelButton.addEventListener){
-            cancelButton.addEventListener("click", clearMarkDownPreview, false);
-        }
-        if(saveButton.addEventListener){
-            saveButton.addEventListener("click", clearMarkDownPreview, false);
-        }
-    }
-
-} else if (!window.ran && location.href.indexOf('/apex/adm_userstoryedit') > -1 && location.href.indexOf('gus.lightning.force') > -1) {
-    console.log('userstoryedit lightning'); // eslint-disable-line no-console
-    var element = document.getElementById('userStoryEdit:j_id0:workSds:storyWorkForm:descriptionInput:inputComponent:inputFieldWithContainer');
-    var destinationElement = element.parentElement;
-    editingPage(element, destinationElement);
-
-} else if (!window.ran && location.href.indexOf('/apex/adm_userstoryedit') > -1 && location.href.indexOf('gus.lightning.force') == -1) {
-    console.log('userstoryedit classic'); // eslint-disable-line no-console
-    var element = document.getElementById('userStoryWorkPage:storyWorkForm:detailsInput:formRow:input');
-    var destinationElement = element.parentElement;
-    editingPage(element, destinationElement);
-
-} else if (!window.ran && location.href.indexOf('/apex/adm_userstorydetail') > -1 && location.href.indexOf('gus.lightning.force') > -1) {
-    console.log('userstorydetail lightning'); // eslint-disable-line no-console
-    viewingPage('userStoryDetailPage_userStoryWorkForm_detailsInput_inputComponent_outputStandalone_ileinner');
-
-} else if (!window.ran && location.href.indexOf('/apex/adm_userstorydetail') > -1 && location.href.indexOf('gus.lightning.force') == -1) {
-    console.log('userstorydetail classic'); // eslint-disable-line no-console
-    viewingPage('userStoryDetailPage_userStoryWorkForm_detailsInput_inputComponent_outputStandalone_ileinner');
-
-} else if (!window.ran && location.href.indexOf('/apex/adm_bugdetail') > -1 && location.href.indexOf('gus.lightning.force') > -1) {
-    console.log('bugdetail lightning'); // eslint-disable-line no-console
-    viewingPage('bugDetailPage:bugWorkForm:j_id89bugDetailPage:bugWorkForm:j_id89_00NB0000000FiIs_div');
-} else if (!window.ran && location.href.indexOf('/apex/adm_bugdetail') > -1 && location.href.indexOf('gus.lightning.force') == -1) {
-    console.log('bugdetail classic'); // eslint-disable-line no-console
-    viewingPage('bugDetailPage:bugWorkForm:j_id89bugDetailPage:bugWorkForm:j_id89_00NB0000000FiIs_div');
-} else {
-    console.log('not found'); // eslint-disable-line no-console
 }
 
-window.ran = true;
+initialize(run);
